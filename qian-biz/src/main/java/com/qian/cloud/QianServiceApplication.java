a212b53372f445d4e123e9662d750f1da7bcfae2
qian <fengqian2018@dingtalk.com>
2020-06-16 14:32:25 +0800

    first-pom

---
+++ qian-biz/src/main/java/com/qian/cloud/QianServiceApplication.java
@@ -0,0 +1,16 @@
+package com.qian.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * Hello world!
+ *
+ * @author qian
+ */
+@SpringCloudApplication
+public class QianServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QianServiceApplication.class, args);
+    }
+}
